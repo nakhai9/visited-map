a4ed761cd4464d84848f9b9bfb3ba369498ce3ba
Khai Nguyen <khai.fordev@gmail.com>
2026-09-17 16:58:10 +0700

    add title

--- app/src/pages/demo/Demo.tsx
+++ app/src/pages/demo/Demo.tsx
@@ -8,12 +8,12 @@ export default function Demo() {
   const [states, setStates] = useState<StateEvent[]>([]);
   return (
     <Layout>
+      <Box sx={{ height: { xs: 12, sm: 24 } }}>
+        <Typography variant="h2">Những nơi đã đến</Typography>
+      </Box>
       <Grid spacing={2} container>
         <Grid size={{ md: 3, sm: 1, xs: 12 }}></Grid>
         <Grid size={{ md: 6, sm: 10, xs: 12 }}>
-          <Box sx={{ height: { xs: 12, sm: 24 } }}>
-            <Typography variant="h2">Những nơi đã đến</Typography>
-          </Box>
           <MapView onChange={(states) => setStates(states)} />
         </Grid>
         <Grid size={{ md: 3, sm: 1, xs: 12 }}></Grid>
